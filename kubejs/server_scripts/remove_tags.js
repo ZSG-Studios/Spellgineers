76e6dae4c388c14780c6c722d0c31c7ff41823c0
Zachary Rose <zacharydavidrose1998@gmail.com>
2025-01-25 05:18:30 -0500

    A

--- kubejs/server_scripts/remove_tags.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.tags('item', event => {
-    // Remove invalid or missing item from the tag
-    event.remove('chisel_chipped_integration:laboratory_block', 'chisel_chipped_integration:laboratory_colsole')
-})
-
-ServerEvents.tags('block', event => {
-    // Remove invalid or missing blocks from the tag
-    event.remove('chisel_chipped_integration:laboratory_block', 'chisel_chipped_integration:laboratory_colsole')
-})
